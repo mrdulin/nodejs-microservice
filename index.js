808948d53bca44d543033c647986d21492dcb85d
mrdulin <novaline@qq.com>
2018-03-19 22:47:52 +0800

    添加seneca-basic, seneca-entity插件

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const seneca = require('seneca')();
 const SenecaWeb = require('seneca-web');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressAdapter = require('seneca-web-adapter-express');
 
 const routes = require('./routes');
 const productPlugin = require('./plugins/product');
@@ -17,14 +18,17 @@ seneca
   //   host: '127.0.0.1',
   //   port: '27017'
   // })
-  // .use(productPlugin)
+  .use('basic')
+  .use('entity')
+  .use(productPlugin)
   .use(SenecaWeb, {
     routes,
     context: app,
-    adapter: require('seneca-web-adapter-express'),
+    adapter: expressAdapter,
     options: { parseBody: false }
   })
   .ready(err => {
+    if (err) throw err;
     const server = seneca.export('web/context')();
 
     server.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 module.exports = [
   {
     prefix: '/products',
-    pin: { area: 'product', action: '*' },
+    pin: 'area:product,action:*',
+    // pin: { area: 'product', action: '*' },
     map: {
       fetch: { GET: true },
       edit: { GET: false, POST: true },
